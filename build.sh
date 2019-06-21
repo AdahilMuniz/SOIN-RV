24c67237424694de5a9ab8e360889dece227ca55
AdahilMuniz <adahilmuniz@hotmail.com>
2019-06-21 23:31:23 +0200

    Integrating TCL script

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
-FILE="code_examples/RV32I/$2.rv32i"
+ROOT_DIR="$(pwd)"
+FILE="$ROOT_DIR/code_examples/RV32I/$2.rv32i"
+SCRIPT_DIR="$ROOT_DIR/script"
 
 ./code_examples/build.sh $1 $2
 
 vlib work
-vlog -sv +acc +incdir+include src/RV32I.sv
-vlog -sv +acc test/SystemVerilog/interfaces/test_if.sv
-vlog -sv +acc test/SystemVerilog/interfaces/memory_if.sv
-vlog -sv +acc test/SystemVerilog/interfaces/reg_file_if.sv
-vlog -sv +acc +incdir+include +incdir+test/SystemVerilog/model +incdir+test/SystemVerilog/environment test/SystemVerilog/tb_pkg.svh
-vlog -sv +acc +incdir+include +incdir+test/SystemVerilog/interfaces test/SystemVerilog/tb/tb.sv
-vsim -classdebug -gIM_FILE="$FILE" -do test/wave.do tb
+
+vsim \
+-do "set FILE $FILE" \
+-do "set ROOT_DIR $ROOT_DIR" \
+-do "set SCRIPT_DIR $SCRIPT_DIR" \
+-do "script/tcl/common/list_design.tcl" \
+-do "script/tcl/common/list_tb.tcl" \
+-do "script/tcl/mentor/compile.tcl" \
+-do "script/tcl/mentor/load.tcl" \
+-do "script/tcl/mentor/run.tcl"
