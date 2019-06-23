29b08c0c9f55f3ed2d12780ca2eda7e055aac771
AdahilMuniz <adahilmuniz@hotmail.com>
2019-06-23 12:07:55 +0200

    Integrating sw compile script to the main build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 ROOT_DIR="$(pwd)"
 
-FILE="$ROOT_DIR/sw/RV32I/$2.rv32i"
+FILE_TO_LOAD="$ROOT_DIR/sw/RV32I/$2.rv32i"
+FILE_TO_COMPILE="$ROOT_DIR/sw/assembly/$2.S"
 
 SCRIPT_DIR="$ROOT_DIR/script"
 
@@ -12,7 +13,7 @@ if [[ $GUI -eq "0" ]]; then
 	ARGS="$ARGS -c"
 fi
 
-./sw/build.sh $1 $2
+#./sw/build.sh $1 $2
 
 if [ ! -d "work" ]
 then
@@ -21,9 +22,11 @@ fi
 
 vsim $ARGS \
 -do "set GUI $GUI" \
--do "set FILE $FILE" \
+-do "set FILE_TO_COMPILE $FILE_TO_COMPILE" \
+-do "set FILE_TO_LOAD $FILE_TO_LOAD" \
 -do "set ROOT_DIR $ROOT_DIR" \
 -do "set SCRIPT_DIR $SCRIPT_DIR" \
+-do "script/tcl/sw/compile.tcl" \
 -do "script/tcl/mentor/env.tcl" \
 -do "script/tcl/common/list_design.tcl" \
 -do "script/tcl/common/list_tb.tcl" \
